77abc6e678a171a2bd16a7b8d00cb612dc54aebe
Anonymous <bajwab4u@gmail.com>
2021-02-16 15:54:01 +0500

    Sending Email of teacher with Create Class Request

--- src/app/Dashboard/teacher-dashboard/tclasses/tclasses.component.ts
+++ src/app/Dashboard/teacher-dashboard/tclasses/tclasses.component.ts
@@ -4,6 +4,7 @@ import { ClassService } from '../../../Services/class.service';
 import {Router} from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
+import { ProfileService } from './../../../Services/profile.service';
 
 @Component({
   selector: 'app-tclasses',
@@ -12,20 +13,32 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class TClassesComponent implements OnInit {
 
-  constructor(private _service:ClassService,private toastr: ToastrService,private router:Router,private pageTitle:Title) { }
+  constructor(private _service:ClassService,private toastr: ToastrService,private router:Router,private pageTitle:Title,private profileService:ProfileService) { }
   errorObj:any={
     name:'',
-    email:'',
-    password:'',
-    category:''
+    description:'',
+    id:''
   };
+  email;
   ngOnInit(): void {
     this.pageTitle.setTitle('Create Class');
+    
+    this.profileService.getusername().subscribe(data=>{
+      this.email = data["email"];
+      },
+      err=>{
+        this.router.navigate['login'];
+      })
   }
+  
+
+
   onSubmit(form:NgForm){
+    form.value["email"]=this.email;
     this._service.createClass(form.value).subscribe(
       data => {console.log(data);
         this.toastr.success('Class Successfully Created',);
+        console.log(form.value);
       },
       err => {
         this.errorObj = err.error;
